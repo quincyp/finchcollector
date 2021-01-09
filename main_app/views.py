7c198fbff9b5172d115ed337d81b18fbb10b384b
qtpham <quincytpham@gmail.com>
2021-01-08 16:30:25 -0800

    Mod home route to /home, added index and about route

--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
